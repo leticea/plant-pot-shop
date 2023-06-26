053539e3bae37f71511cb6c3087583151bf70e2f
leticea <nascimento.leticeah@gmail.com>
2023-06-26 08:54:33 -0300

    feat(DefaultLayout): creating default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
-import { Header } from "./components/Header";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Header />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -3,8 +3,10 @@ import { Header } from "../../components/Header";
 import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
-  <LayoutContainer>
-    <Header />
-    <Outlet />
-  </LayoutContainer>
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  );
 }
