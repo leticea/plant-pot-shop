33497411e9c0682f76b5611989e17bc73c51a958
leticea <nascimento.leticeah@gmail.com>
2023-06-30 13:51:03 -0300

    feat(PotsContext): creating contexts

--- src/contexts/PotsContext.tsx
+++ src/contexts/PotsContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useState } from "react";
 import { PlantPotsProps } from "../pages/Home/PotsList/components/PotsCard";
+import { produce } from "immer";
 
 export interface CartItem extends PlantPotsProps {
   quantity: number;
@@ -9,6 +10,7 @@ interface CartContextType {
   cartItems: CartItem[];
   cartQuantity: number;
   totalCartItems: number;
+  addPotToCart: (pot: CartItem) => void;
 }
 
 export const CartContext = createContext({} as CartContextType);
@@ -17,6 +19,44 @@ interface CartContextProviderProps {
   children: ReactNode;
 }
 
+const POTS_ITEMS_STORAGE_KEY = "plantPotShop:cartItems";
+
 export function CartContextProvider({ children }: CartContextProviderProps) {
-  const [cartItems, setCartItems] = useState<CartItem[]>();
+  const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = localStorage.getItem(POTS_ITEMS_STORAGE_KEY);
+    if (storedCartItems) {
+      return JSON.parse(storedCartItems);
+    }
+    return [];
+  });
+
+  const cartQuantity = cartItems.length;
+
+  const totalCartItems = cartItems.reduce((total, cartItem) => {
+    return total + cartItem.price * cartItem.quantity;
+  }, 0);
+
+  function addPotToCart(pot: CartItem) {
+    const potAlreadyExistsInCart = cartItems.findIndex(
+      (cartItem) => cartItem.id === pot.id
+    );
+
+    const newOrder = produce(cartItems, (draft) => {
+      if (potAlreadyExistsInCart < 0) {
+        draft.push(pot);
+      } else {
+        draft[potAlreadyExistsInCart].quantity += pot.quantity;
+      }
+    });
+
+    setCartItems(newOrder);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ addPotToCart, cartItems, cartQuantity, totalCartItems }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
