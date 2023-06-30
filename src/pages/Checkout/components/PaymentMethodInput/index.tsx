081b4acb4e161a744a27f362c4e0e46324d1c0af
leticea <nascimento.leticeah@gmail.com>
2023-06-30 10:52:27 -0300

    feat(PaymentMethodInput): creating component

--- src/pages/Checkout/components/PaymentMethodInput/index.tsx
+++ src/pages/Checkout/components/PaymentMethodInput/index.tsx
@@ -1,16 +1,24 @@
-import { InputHTMLAttributes, ReactNode, forwardRef } from "react"
-import { PaymentMethodContainer } from "./styles";
+import { InputHTMLAttributes, ReactNode, forwardRef } from "react";
+import { Content, PaymentMethodContainer } from "./styles";
 
 type PaymentMethodInputProps = InputHTMLAttributes<HTMLInputElement> & {
   icon: ReactNode;
   label: string;
-}
+};
 
-export const PaymentMethodInput = forwardRef<HTMLInputElement, PaymentMethodInputProps>(({ id, icon, label, ...props }, ref) => {
+export const PaymentMethodInput = forwardRef<
+  HTMLInputElement,
+  PaymentMethodInputProps
+>(({ id, icon, label, ...props }, ref) => {
   return (
     <PaymentMethodContainer>
-
+      <input id={id} type="radio" {...props} name="paymentMethod" ref={ref} />
+      <label htmlFor={id}>
+        <Content>
+          {icon}
+          {label}
+        </Content>
+      </label>
     </PaymentMethodContainer>
-  )
-})
-
+  );
+});
--- src/pages/Checkout/components/PaymentMethodInput/styles.ts
+++ src/pages/Checkout/components/PaymentMethodInput/styles.ts
@@ -17,3 +17,28 @@ export const PaymentMethodContainer = styled.div`
     `}
   }
 `;
+
+export const Content = styled.div`
+  padding: 0 1rem;
+  margin: 3.2rem 0;
+  background: ${(props) => props.theme["gray-400"]};
+  color: ${(props) => props.theme["gray-700"]};
+  display: flex;
+  align-items: center;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  height: 5rem;
+  width: 17rem;
+  border-radius: 6px;
+  border: 1px solid ${(props) => props.theme["gray-400"]};
+  transition: 0.3s;
+  user-select: none;
+
+  svg {
+    color: ${(props) => props.theme["green-600"]};
+  }
+
+  &:hover {
+    background: ${(props) => props.theme["gray-500"]};
+  }
+`;
